cf7370f3602347134fdd4956fe278285beaa70dc
shinilornekora <arokenrol@mail.ru>
2023-07-16 16:59:15 +0300

    stable version 1.0

--- src/App.js
+++ src/App.js
@@ -3,46 +3,82 @@ import { useEffect, useState } from "react";
 import api from './apis'
 
 function App() {
-  const [currentTable, setCurrentTable] = useState('');
-  const [tableInfo, setTableInfo] = useState([]);
-  useEffect( () => {
-      async function fetchData() {
-          try {
-              if (api[currentTable]) {
-                  const result = await api[currentTable].element;
-                  setTableInfo(result.data);
-              }
-          } catch (error) {
-              console.log(error);
-          }
-      }
-      fetchData();
-  }, [currentTable])
-  return (
-    <div className="App">
-      <select onChange={(event) => setCurrentTable(event.target.value)} name="service" id="entities">
-        <option value="character">Персонажи</option>
-        <option value="quest">Задания</option>
-        <option value="location">Локация</option>
-        <option value="item">Вещи</option>
-      </select>
-      <div className="mainInfo">
-        {
-          tableInfo?.map((element) => {
-            return (
-                <div key={element} className="element__container">
-                  {Object.entries(element)[0]}
-                    <div className="actionButtons">
-                        <button className="action__1">Посмотреть</button>
-                        <button className="action__2">Удалить</button>
-                    </div>
-                </div>
-            );
-
-          })
+    const [currentTable, setCurrentTable] = useState('');
+    const [tableInfo, setTableInfo] = useState([]);
+    useEffect( () => {
+        async function fetchData() {
+            try {
+                if (currentTable !== 'no-value') {
+                    const result = await api.table.element(currentTable);
+                    switch(currentTable) {
+                        case 'character':
+                            setTableInfo(result.data.map((e) => {
+                                console.log(e.name);
+                                return e.name
+                            }));
+                            break;
+                        case 'quest':
+                            setTableInfo(result.data.map((e) => {
+                                return e.name;
+                            }))
+                            break;
+                        case 'location':
+                            setTableInfo(result.data.map((e) => {
+                                return e.name;
+                            }))
+                            break;
+                        case 'item':
+                            setTableInfo(result.data.map((e) => {
+                                return e.name;
+                            }))
+                            break;
+                        default:
+                          setTableInfo([]);
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+                setTableInfo([]);
+            }
         }
-      </div>
-    </div>
+        fetchData();
+    }, [currentTable])
+
+    function deleteQuery(params) {
+      try {
+          api.table.delete(currentTable, params);
+      } catch (error) {
+          console.log(error)
+      }
+    }
+
+  return (
+      <div className="App">
+           <div className="input__field">
+               <select onChange={(event) => setCurrentTable(event.target.value)} name="service" id="entities">
+                   <option value="no-value" defaultValue></option>
+                   <option value="character">Персонажи</option>
+                   <option value="quest">Задания</option>
+                   <option value="location">Локация</option>
+                   <option value="item">Вещи</option>
+               </select>
+               <div className="mainInfo">
+                   { tableInfo.length ?
+                       tableInfo.map((element) => {
+                           return (
+                               <div key={element} className="element__container">
+                                   {element}
+                                   <div className="actionButtons">
+                                       <button className="action__1">Посмотреть</button>
+                                       <button onClick={() => deleteQuery({key: element})}  className="action__2">Удалить</button>
+                                   </div>
+                               </div>
+                           );
+                            }) : 'Информации нет.'
+                   }
+               </div>
+           </div>
+       </div>
   );
 }
 
--- src/apis.js
+++ src/apis.js
@@ -1,29 +1,16 @@
 import axios from 'axios'
-
+const BASE_URL = 'http://192.168.0.108:8090'
 const api = {
-    character: {
-        element: axios.get('http://localhost:8090/api/table/character/').catch((error) => error),
-        add: (params) => axios.post('http://localhost:8090/api/table/character/add', params),
-        delete: (params) => axios.post('api/table/character/delete', params),
-        update: (params) => axios.post('api/table/character/update', params),
-    },
-    quest: {
-        element: axios.get('http://localhost:8090/api/table/quest/').catch((error) => error),
-        add: (params) => axios.post('api/table/quest/add', params),
-        delete: (params) => axios.post('api/table/quest/delete', params),
-        update: (params) => axios.post('api/table/quest/update', params),
-    },
-    location: {
-        element: axios.get('http://localhost:8090/api/table/location/').catch((error) => error),
-        add: (params) => axios.post('http://localhost:8090/api/table/location/add', params),
-        delete: (params) => axios.post('http://localhost:8090/api/table/location/delete', params),
-        update: (params) => axios.post('http://localhost:8090/api/table/location/update', params),
-    },
-    item: {
-        element: axios.get('http://localhost:8090/api/table/item').catch((error) => error),
-        add: (params) => axios.post('http://localhost:8090/api/table/item/add', params),
-        delete: (params) => axios.post('http://localhost:8090/api/table/item/delete', params),
-        update: (params) => axios.post('http://localhost:8090/api/table/item/update', params),
+    table: {
+        element: (table) => axios.get(`${BASE_URL}/api/table/${table}/`).catch((error) => error),
+        add: (table, params) => axios.post(`${BASE_URL}/api/table/${table}/add`, params),
+        delete: (table, params) => axios.delete(`${BASE_URL}/api/table/${table}/delete`, {
+            headers: {
+                "code": "OK",
+            },
+            data: params
+        }),
+        update: (table, params) => axios.put(`${BASE_URL}/api/table/${table}/update`, params),
     },
 }
 
